9041d0fbd9270ed6b39e5ea90738e322115fbce7
jxh <1059751314@qq.com>
2019-02-21 16:46:42 +0800

    用户个人页原型

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
