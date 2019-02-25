5a537ade9cd5cefa4ea6c427d9cd3540c3c9c6af
jxh <1059751314@qq.com>
2019-02-25 18:21:02 +0800

    显示个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,16 +8,32 @@ use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    /**
+     * 显示页面
+     * @param User $user
+     * @return View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+    /**
+     * 修改页面
+     * @param User $user
+     * @return View
+     */
     public function edit(User $user)
     {
         return view('users.edit', compact('user'));
     }
 
+    /**
+     * 修改信息操作
+     * @param UserRequest $request
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(UserRequest $request, User $user)
     {
         $user->update($request->all());
