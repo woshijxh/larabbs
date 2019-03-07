cf29e3d0c959ab5d8f106e8d5f1acb6eff2f7211
jxh <1059751314@qq.com>
2019-03-07 15:48:37 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,17 +3,27 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
+
+    public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->content = clean($reply->content, 'user_topic_body');
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
+    }
+
+
+    public function creating(Reply $reply)
+    {
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 
     public function updating(Reply $reply)
