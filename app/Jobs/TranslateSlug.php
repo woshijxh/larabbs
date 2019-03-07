0a8adde4654271b3035a12cc17d5fcc7ba241c61
jxh <1059751314@qq.com>
2019-03-07 09:46:39 +0800

    回复列表

--- app/Jobs/TranslateSlug.php
+++ app/Jobs/TranslateSlug.php
@@ -27,9 +27,9 @@ class TranslateSlug implements ShouldQueue
     {
         // 未解决
         // 请求百度 API 接口进行翻译
-        //$slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
+        $slug = app(SlugTranslateHandler::class)->translate($this->topic->title);
 
         // 为了避免模型监控器死循环调用，我们使用 DB 类直接对数据库进行操作
-        // \DB::table('topics')->where('id', $this->topic->id)->update(['slug' => $slug]);
+        \DB::table('topics')->where('id', $this->topic->id)->update(['slug' => 'fsf']);
     }
 }
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -19,7 +19,7 @@ class TopicObserver
         $topic->excerpt = make_excerpt($topic->body);
 
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
+        if ($topic->slug) {
 
             // 推送任务到队列
             dispatch(new TranslateSlug($topic));
