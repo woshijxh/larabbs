282b369b85af045a63974ab0f0e634fd76d49839
jxh <1059751314@qq.com>
2019-03-27 11:47:51 +0800

    notifications index

--- routes/api.php
+++ routes/api.php
@@ -113,7 +113,9 @@ $api->version('v1', [
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
                 ->name('api.topics.replies.destroy');
 
-
+            // 通知列表
+            $api->get('user/notifications', 'NotificationsController@index')
+                ->name('api.user.notifications.index');
         });
     });
 });
